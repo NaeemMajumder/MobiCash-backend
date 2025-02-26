fadedee6b083390218d859f525a55f44b0bec6b8
naeem majumder <naeemmajumder00@gmail.com>
2025-02-26 17:02:40 +0600

    connect mongoose

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config();
+}
+
 // import
 const express = require('express');
 const cors = require("cors");
 const app = express();
+
+// mongoose (2)
+const mongoose = require("mongoose");
+let mongo_url = "mongodb://127.0.0.1:27017/LamiaEnterprise";
+main().then(()=>{
+    console.log("mongodb is connected");
+}).catch((error)=>{
+    console.log(error);
+})
+async function main() {
+    await mongoose.connect(mongo_url);
+}
 
 
 app.use(express.json());
